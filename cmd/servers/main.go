0ad592519959261db357908d1e0b7ec732a15415
felipeazsantos <felipeasantos10@hotmail.com>
2025-06-18 11:26:34 -0300

    creating gRPC server

--- cmd/servers/main.go
+++ cmd/servers/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 
 	"github.com/felipeazsantos/pos-goexpert/fc-clean-arch-challenge/configs"
 	"github.com/felipeazsantos/pos-goexpert/fc-clean-arch-challenge/internal/infra/database"
+	"github.com/felipeazsantos/pos-goexpert/fc-clean-arch-challenge/internal/infra/grpc/pb"
+	"github.com/felipeazsantos/pos-goexpert/fc-clean-arch-challenge/internal/infra/grpc/service"
 	"github.com/felipeazsantos/pos-goexpert/fc-clean-arch-challenge/internal/infra/web"
 	"github.com/felipeazsantos/pos-goexpert/fc-clean-arch-challenge/internal/infra/web/webserver"
 	"github.com/felipeazsantos/pos-goexpert/fc-clean-arch-challenge/internal/usecase"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -33,5 +38,17 @@ func main() {
 
 	fmt.Println("Starting web server on port:", cfg.WebServerPort)
 	go webserver.Start()
+
+	grpcServer := grpc.NewServer()
+	orderService := service.NewOrderService(createOrderUseCase, listOrdersUseCase)
+	pb.RegisterOrderServiceServer(grpcServer, orderService)
+	reflection.Register(grpcServer)
+
+	fmt.Println("Starting gRPC server on port:", cfg.GrpcServerPort)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.GrpcServerPort))
+	if err != nil {
+		log.Fatal(fmt.Sprintf("failed to listen on port %s:", cfg.GrpcServerPort), err)
+	}
+	go grpcServer.Serve(lis)
 
 }
